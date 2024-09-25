515b41c0cb8d2664adf7c6180ce1a6d8c034d667
Gunbacca <albe07001@utb.vaxjo.se>
2024-09-25 11:18:41 +0200

    klar med uppgift 1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿/*double radie=2.4;
+Console.WriteLine("Cirkelns omkrets: "+CirkelOmkrets(radie));
+Console.WriteLine("Cirkelns area: "+CirkelArea(radie));
+
+double CirkelOmkrets(double r){
+    return(r*2*Math.PI);
+}
+double CirkelArea(double r){
+return(r*r*Math.PI);
+}
+*/
+
